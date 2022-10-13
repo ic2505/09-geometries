5aec0d44faeec4129d2ea99de3e42c0f8e7621a0
Ilker Ciliv <ilkerciliv@gmail.com>
2022-10-13 14:38:06 -0400

    rendering different geometiries as well as 1 triangle

---
+++ src/script.js
@@ -0,0 +1,103 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+
+// Mesh 1
+const geometry = new THREE.TorusGeometry(10, 1, 10, 100);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Mesh 2
+const geometry2 = new THREE.OctahedronGeometry(3, 6, 6, 8, 8, 8);
+const mesh2 = new THREE.Mesh(geometry2, material);
+scene.add(mesh2);
+
+//Drawing a triangle
+// vertex array, passed as Float32Array. 3 verteces with x,y,x values
+const positionsArray = new Float32Array([
+  0, 0, 0, 
+  0, 1, 0, 
+  1, 0, 0
+]);
+// 1 vertex contains 3 values
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+const geometry3 = new THREE.BufferGeometry();
+geometry3.setAttribute("position", positionsAttribute);
+const mesh3 = new THREE.Mesh(geometry3, material);
+scene.add(mesh3);
+
+mesh3.position.x = 6;
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
